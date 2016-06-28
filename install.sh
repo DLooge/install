3252fa1fd4eb8fd6662e57c70edf5c3251a0551b
Ian Wainwright <ian.wainwright@hpcsweden.se>
2016-06-28 13:48:05 +0200

    Adding first version of install script.

---
+++ install.sh
@@ -0,0 +1,9 @@
+
+# For things needed to run nbody (as it has graphics):
+sudo apt-get install freeglut3-dev build-essential libx11-dev libxmu-dev libxi-dev libgl1-mesa-glx libglu1-mesa libglu1-mesa-dev
+
+# For cpu temperature monitoring:
+sudo apt-get install lm-sensors
+
+# For running monitors:
+sudo apt-get install xterm
